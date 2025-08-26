c6fe9ff80c455685bb20eb17d4906dc9c5e54cb3
ilumrhisoka <baabaabooooey@gmail.com>
2025-08-27 00:23:25 +0300

    corrections

---
+++ target/generated-sources/annotations/com/example/bankcards/util/mapper/TransferMapperImpl.java
@@ -0,0 +1,32 @@
+package com.example.bankcards.util.mapper;
+
+import com.example.bankcards.model.dto.transfer.TransferResponseDto;
+import com.example.bankcards.model.entity.Transfer;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-08-26T22:04:27+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.6 (Oracle Corporation)"
+)
+@Component
+public class TransferMapperImpl implements TransferMapper {
+
+    @Override
+    public TransferResponseDto toTransferResponseDto(Transfer transfer) {
+        if ( transfer == null ) {
+            return null;
+        }
+
+        TransferResponseDto transferResponseDto = new TransferResponseDto();
+
+        transferResponseDto.setId( transfer.getId() );
+        transferResponseDto.setAmount( transfer.getAmount() );
+        transferResponseDto.setStatus( transfer.getStatus() );
+        transferResponseDto.setTransferDate( transfer.getTransferDate() );
+        transferResponseDto.setCreatedAt( transfer.getCreatedAt() );
+
+        return transferResponseDto;
+    }
+}
