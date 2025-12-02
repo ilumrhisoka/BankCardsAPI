e0abc633617f346da023b105f5100906d6eeb374
ilumrhisoka <baabaabooooey@gmail.com>
2025-12-02 22:03:49 +0300

    converter added

---
+++ target/generated-sources/annotations/com/example/bankcards/util/mapper/CardMapperImpl.java
@@ -0,0 +1,54 @@
+package com.example.bankcards.util.mapper;
+
+import com.example.bankcards.model.dto.card.CardResponseDto;
+import com.example.bankcards.model.entity.Account;
+import com.example.bankcards.model.entity.Card;
+import com.example.bankcards.model.entity.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-02T21:59:57+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.6 (Oracle Corporation)"
+)
+@Component
+public class CardMapperImpl implements CardMapper {
+
+    @Override
+    public CardResponseDto toCardResponseDto(Card card) {
+        if ( card == null ) {
+            return null;
+        }
+
+        CardResponseDto cardResponseDto = new CardResponseDto();
+
+        cardResponseDto.setUsername( cardAccountUserUsername( card ) );
+        cardResponseDto.setId( card.getId() );
+        cardResponseDto.setCardHolder( card.getCardHolder() );
+        cardResponseDto.setExpiryDate( card.getExpiryDate() );
+        cardResponseDto.setCardStatus( card.getCardStatus() );
+        cardResponseDto.setBalance( card.getBalance() );
+
+        return cardResponseDto;
+    }
+
+    private String cardAccountUserUsername(Card card) {
+        if ( card == null ) {
+            return null;
+        }
+        Account account = card.getAccount();
+        if ( account == null ) {
+            return null;
+        }
+        User user = account.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        String username = user.getUsername();
+        if ( username == null ) {
+            return null;
+        }
+        return username;
+    }
+}
---
+++ target/generated-sources/annotations/com/example/bankcards/util/mapper/TicketMapperImpl.java
@@ -0,0 +1,66 @@
+package com.example.bankcards.util.mapper;
+
+import com.example.bankcards.model.dto.ticket.TicketResponseDto;
+import com.example.bankcards.model.entity.SupportTicket;
+import com.example.bankcards.model.entity.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-02T21:59:57+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.6 (Oracle Corporation)"
+)
+@Component
+public class TicketMapperImpl implements TicketMapper {
+
+    @Override
+    public TicketResponseDto toTicketResponseDto(SupportTicket ticket) {
+        if ( ticket == null ) {
+            return null;
+        }
+
+        TicketResponseDto ticketResponseDto = new TicketResponseDto();
+
+        ticketResponseDto.setCreatedByUsername( ticketUserUsername( ticket ) );
+        ticketResponseDto.setAssignedToAdminUsername( ticketAdminAssignedUsername( ticket ) );
+        ticketResponseDto.setId( ticket.getId() );
+        ticketResponseDto.setSubject( ticket.getSubject() );
+        ticketResponseDto.setDescription( ticket.getDescription() );
+        ticketResponseDto.setStatus( ticket.getStatus() );
+        ticketResponseDto.setPriority( ticket.getPriority() );
+        ticketResponseDto.setCreatedAt( ticket.getCreatedAt() );
+
+        return ticketResponseDto;
+    }
+
+    private String ticketUserUsername(SupportTicket supportTicket) {
+        if ( supportTicket == null ) {
+            return null;
+        }
+        User user = supportTicket.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        String username = user.getUsername();
+        if ( username == null ) {
+            return null;
+        }
+        return username;
+    }
+
+    private String ticketAdminAssignedUsername(SupportTicket supportTicket) {
+        if ( supportTicket == null ) {
+            return null;
+        }
+        User adminAssigned = supportTicket.getAdminAssigned();
+        if ( adminAssigned == null ) {
+            return null;
+        }
+        String username = adminAssigned.getUsername();
+        if ( username == null ) {
+            return null;
+        }
+        return username;
+    }
+}
